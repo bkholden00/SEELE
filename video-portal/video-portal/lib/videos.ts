2ce268d1d4e33b77fdec789929e816c7d6275ea5
bkholden00 <bkholden00@gmail.com>
2026-09-06 18:23:45 -0400

    Update videos.ts

--- video-portal/video-portal/lib/videos.ts
+++ video-portal/video-portal/lib/videos.ts
@@ -10,32 +10,37 @@ export const videos: Video[] = [
     slug: "module-0",
     title: "Module 0 – Introduction",
     description: "Introduction to the Morphological Knowledge Module Series.",
-    url: "https://youtu.be/2QT7vOAby1M",
+    url: "https://youtu.be/HjsDzWMYD90",
   },
   {
     slug: "module-1",
     title: "Module 1 – Breaking Down Words Into Word Parts",
-    url: "https://youtu.be/Q8jl_tYAILg",
+    url: "https://youtu.be/vYRNuxv04uE",
   },
   {
     slug: "module-2",
     title: "Module 2 – Building Words From Word Parts",
-    url: "https://youtu.be/LpkPwulkPCc",
+    url: "https://youtu.be/Cr3LUqpgB-M",
   },
   {
     slug: "module-3",
     title: "Module 3 – Highlighting Semantic and Grammatical Shifts",
-    url: "https://youtu.be/nJk5_6tsztk",
+    url: "https://youtu.be/ml2QYD3wf80",
   },
   {
     slug: "module-4",
     title: "Module 4 – Understanding Derived Words",
-    url: "https://youtu.be/S4Xg5wZjc5E",
+    url: "https://youtu.be/b5NyV4zXcm8",
   },
   {
     slug: "module-5",
     title: "Module 5 – Spelling and Phonological Shifts",
-    url: "https://youtu.be/mU48LwaczoA",
+    url: "https://youtu.be/tZOPOt4g-Qg",
+  },
+  {
+    slug: "module-6",
+    title: "Module 6 – Giving Students Opportunities to Deduce the Meaning of New Words",
+    url: "https://youtu.be/R9ovmok7ElA",
   },
 ];
 
